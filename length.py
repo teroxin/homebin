b5c0b32bc95b64fa613a136323e6715e0b7efd2b
Eric Betts <bettse@gmail.com>
2013-04-07 21:30:06 -0700

    remove sub-day increments, add correct grammar (singular, plural)

--- length.py
+++ length.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
 from datetime import datetime, timedelta
+import inflect
+p = inflect.engine()
+
 then = datetime.strptime('Nov 21 2011', '%b %d %Y')
 now = datetime.now()
 diff = now - then
@@ -8,5 +11,10 @@ years = diff.days/365
 diff = diff - timedelta(days=365*years)
 months = diff.days/30
 diff = diff - timedelta(days=30*months)
+days = diff.days
 
-print "About %s years, %s months, %s" % (years, months, diff)
+grammar = {'years': p.plural_noun('year', years),
+        'months': p.plural_noun('month', months),
+        'days': p.plural_noun('day', days)}
+
+print "It has been about %s %s, %s %s, %s %s" % (years, grammar['years'], months, grammar['months'], days, grammar['days'])
